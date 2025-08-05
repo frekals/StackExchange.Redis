4ddbb3f4131579f7a11780d0d5ac929fa0d87d2c
Fredrik Kalseth <fredrik.kalseth@aidn.no>
2025-08-05 20:32:25 +0200

    Repro

---
+++ tests/StackExchange.Redis.Tests/Issues/IssueXXXTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace StackExchange.Redis.Tests.Issues;
+
+public class IssueXXXTests()
+{
+    [Fact]
+    public void Execute()
+    {
+        Parallel.For(0, 100, _ =>
+        {
+            using var conn = ConnectionMultiplexer.Connect(
+                $"abortConnect=false,syncTimeout=1,{TestConfig.Current.SentinelServer},serviceName={TestConfig.Current.SentinelSeviceName}");
+        });
+    }
+}
